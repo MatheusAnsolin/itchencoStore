ff40ca5b2aa16a679830ad86f80f91fe1c7749e0
MatheusAnsolin <matheusp.ansolin@gmail.com>
2024-05-12 09:50:20 -0300

    create base project

---
+++ src/main/java/com/itchencostore/itchencoStore/ItchencoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.itchencostore.itchencoStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItchencoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItchencoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itchencostore/itchencoStore/ItchencoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itchencostore.itchencoStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItchencoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
