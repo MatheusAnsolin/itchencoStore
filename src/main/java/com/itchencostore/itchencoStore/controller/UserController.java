1a05dae2d0f3573c0b05cbea1bd2f7b37187d134
Matheus Padilha Ansolin <56612773+MatheusAnsolin@users.noreply.github.com>
2024-05-18 11:17:47 -0300

    create User classes MVC

---
+++ src/main/java/com/itchencostore/itchencoStore/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.itchencostore.itchencoStore.controller;
+
+import com.itchencostore.itchencoStore.model.User;
+import com.itchencostore.itchencoStore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> listarUsuarios() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/itchencostore/itchencoStore/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.itchencostore.itchencoStore.repository;
+
+import com.itchencostore.itchencoStore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
